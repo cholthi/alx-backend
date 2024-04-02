594aef14d5ef192920a90ee2cb02c6405d36545a
cholthi <cholthipaul@gmail.com>
2024-04-02 08:58:45 +0200

    first task complete

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Provides a function index_range"""
+
+
+def index_range(page: int, page_size) -> tuple:
+    """Returns pagination params into a list of data"""
+    offset = page * page_size
+    limit = page_size
+    return (offset, limit)
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,3 @@
+### Pagination
+
+[0-simple_helper_function.py](0x00-pagination/0-simple_helper_function.py)
