80d7c942816007ee528c678bf8e65e9609fedd07
cholthi <cholthipaul@gmail.com>
2024-04-02 09:04:39 +0200

    first task complete

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,6 @@
 
 def index_range(page: int, page_size) -> tuple:
     """Returns pagination params into a list of data"""
-    offset = page * page_size
-    limit = (page - 1) * page_size
+    limit = page * page_size
+    offset = (page - 1) * page_size
     return (offset, limit)
