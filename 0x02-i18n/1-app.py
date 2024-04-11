4175813a3416257c38fef0bb1b1e45264a0c2d9a
cholthi <cholthipaul@gmail.com>
2024-04-11 11:41:39 +0200

    added flask babel support

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,7 +14,7 @@ babel = Babel(app)
 @app.route('/')
 def index() -> str:
     """index view"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
