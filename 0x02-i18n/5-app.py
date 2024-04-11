c063034c9493555a198321e3f3ef0a009d9adaeb
cholthi <cholthipaul@gmail.com>
2024-04-11 13:39:05 +0200

    added in-memory user store

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Basic flask app which renders a template"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union
+
+
+class Config:
+    """App configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id: str) -> Union[str, None]:
+    """ get user from user store"""
+    return users.get(user_id, None)
+
+
+@app.before_request
+def before_request() -> None:
+    """executed before all views"""
+    if get_user(request.args.get('login_as')):
+        g.user = get_user(request.args.get('login_as'))
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get current locale"""
+    if request.args.get('locale'):
+        return request.args.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """index view"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
