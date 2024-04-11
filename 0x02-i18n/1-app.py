a397fcf956090372f4ff9c0a663b0f9c537aaad1
cholthi <cholthipaul@gmail.com>
2024-04-11 11:38:15 +0200

    added flask babel support

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Basic flask app which renders a template"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from config import Config
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """index view"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,6 @@
+"""Config"""
+class Config:
+    """config"""
+    LANGAUGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
