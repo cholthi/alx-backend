590213003823cdb85c40fd448717c22203744230
cholthi <cholthipaul@gmail.com>
2024-04-11 11:45:06 +0200

    added flask babel support

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,13 @@
 """Basic flask app which renders a template"""
 from flask import Flask, render_template
 from flask_babel import Babel
-from config import Config
+
+
+class Config:
+    """config"""
+    LANGAUGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
